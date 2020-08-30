75254d344a7fff6559dfa5dc6c8fbb5bb35f1038
Sbr <shabbir.endeavor@gmail.com>
2020-08-30 09:50:37 +0530

    Updates

---
+++ precache-manifest.99b08c582655ae565b9051065d1df68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd89fe7fd34fb9ba7374ef5c9fce1ab0",
+    "url": "/Recipe-Site/index.html"
+  },
+  {
+    "revision": "b4c56ea3306f50e82152",
+    "url": "/Recipe-Site/static/css/main.fb1ec48c.chunk.css"
+  },
+  {
+    "revision": "481f7332d608408df4f4",
+    "url": "/Recipe-Site/static/js/2.ebee4c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Site/static/js/2.ebee4c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c56ea3306f50e82152",
+    "url": "/Recipe-Site/static/js/main.77b90eda.chunk.js"
+  },
+  {
+    "revision": "5f0dceafe7fac1b12f55",
+    "url": "/Recipe-Site/static/js/runtime-main.f89fa2f1.js"
+  }
+]);
